6c1f5da3bef0f1468e4ea67cb2ffe63ab6d25217
Esteban Slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2025-04-23 09:54:43 -0700

    squirrel moves faster, acorn falls slower

--- main.go
+++ main.go
@@ -3,43 +3,64 @@ package main
 import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"image"
-	_ "image/png" // Enables PNG decoding
+	_ "image/png"
 	"log"
+	"math/rand"
 	"os"
+	"time"
 )
 
 var (
 	treeImg     *ebiten.Image
 	squirrelImg *ebiten.Image
+	acornImg    *ebiten.Image
 )
 
 type Game struct {
 	playerX float64
+	acornX  float64
+	acornY  float64
 }
 
 func (g *Game) Update() error {
-	
+	// Faster squirrel movement
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		g.playerX -= 2
+		g.playerX -= 4
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		g.playerX += 2
+		g.playerX += 4
 	}
+
+	// Slower acorn falling speed
+	g.acornY += 1.5
+
+	// Reset acorn if it hits the ground
+	if g.acornY > 480 {
+		g.acornY = 0
+		g.acornX = float64(rand.Intn(600))
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	
+	// Tree
 	treeOpts := &ebiten.DrawImageOptions{}
-	treeOpts.GeoM.Scale(0.3, 0.3)         
-	treeOpts.GeoM.Translate(100, 50)      
+	treeOpts.GeoM.Scale(0.3, 0.3)
+	treeOpts.GeoM.Translate(100, 50)
 	screen.DrawImage(treeImg, treeOpts)
 
-	
+	// Squirrel
 	squirrelOpts := &ebiten.DrawImageOptions{}
-	squirrelOpts.GeoM.Scale(0.2, 0.2)           
-	squirrelOpts.GeoM.Translate(g.playerX, 340) 
+	squirrelOpts.GeoM.Scale(0.2, 0.2)
+	squirrelOpts.GeoM.Translate(g.playerX, 340)
 	screen.DrawImage(squirrelImg, squirrelOpts)
+
+	// Acorn
+	acornOpts := &ebiten.DrawImageOptions{}
+	acornOpts.GeoM.Scale(0.1, 0.1)
+	acornOpts.GeoM.Translate(g.acornX, g.acornY)
+	screen.DrawImage(acornImg, acornOpts)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -61,13 +82,20 @@ func loadImage(path string) *ebiten.Image {
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	treeImg = loadImage("assets/tree.png")
 	squirrelImg = loadImage("assets/squirrel.png")
+	acornImg = loadImage("assets/acorn.png")
 
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Catch the Acorn")
 
-	if err := ebiten.RunGame(&Game{playerX: 160}); err != nil { 
+	if err := ebiten.RunGame(&Game{
+		playerX: 160,
+		acornX:  float64(rand.Intn(600)),
+		acornY:  0,
+	}); err != nil {
 		log.Fatal(err)
 	}
 }
